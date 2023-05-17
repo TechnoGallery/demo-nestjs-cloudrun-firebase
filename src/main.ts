bbae566e6715301bb5391a9cbe5698c74f866e67
Vitaliy Kononenko <v@kononenko.tech>
2023-05-17 21:49:41 +0300

    Run docker init to initialize docker files

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
-bootstrap();
+
+const logger: Logger = new Logger('Main');
+
+bootstrap().then(() => logger.log('Application started'));
